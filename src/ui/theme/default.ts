bd938519f76019c78c8c2505cb678fe401458191
Liandro Silva <liandro.silva@outlook.com>
2023-07-17 21:28:15 -0300

    feat: update theme

--- src/ui/theme/default.ts
+++ src/ui/theme/default.ts
@@ -1,13 +1,35 @@
 export const defaultTheme = {
-    colors: {
-        positive: "#1ECB4F",
-        negative: "#FF3441",
-        ["neagtive-medium"]: "#BE3A43",
-        ["neutral-1"]: "#0166FF",
-        ["neutral-4"]: "#00344A",
-        ["gray-5"]: "#737D86",
-        ["gray-6"]: "#5A646E",
-        ["gray-7"]: "#3B444F",
-        ["gray-8"]: "#22272E",
-    }
-}
+  colors: {
+    ["positive"]: "#1ECB4F",
+    ["negative"]: "#FF3441",
+    ["negative-medium"]: "#BE3A43",
+    ["neutral-1"]: "#0166FF",
+    ["neutral-4"]: "#00344A",
+    ["gray-5"]: "#737D86",
+    ["gray-6"]: "#5A646E",
+    ["gray-7"]: "#3B444F",
+    ["gray-8"]: "#22272E",
+    ["pure-white"]: "#FFFFFF",
+  },
+  font: {
+    sizes: {
+      huge: 20,
+      large: 18,
+      normal: 16,
+      small: 14,
+      tiny: 12,
+    },
+    weight: {
+      bold: 700,
+      regular: 400,
+      medium: 500,
+    },
+    lineHeight: {
+      huge: 20 * 1.5,
+      large: 18 * 1.5,
+      normal: 16 * 1.5,
+      small: 14 * 1.5,
+      tiny: 12 * 1.5,
+    },
+  },
+};
