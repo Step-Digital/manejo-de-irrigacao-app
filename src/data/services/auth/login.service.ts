fa63ac63a6c208f5d804468d1894542a11845bfd
Liandro Silva <liandro.silva@outlook.com>
2023-07-17 21:21:23 -0300

    chore: update name LoginService

--- src/data/services/auth/login.service.ts
+++ src/data/services/auth/login.service.ts
@@ -1,7 +1,7 @@
 import { LoginDTO } from "../../dtos/auth";
 import { loginRepository } from "../../repositories/auth";
 
-export const login = async (params: LoginDTO) => {
+export const loginService = async (params: LoginDTO) => {
   try {
     const { data: { data } } = await loginRepository(params);
     return data;
